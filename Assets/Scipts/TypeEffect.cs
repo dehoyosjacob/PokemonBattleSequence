02df09d999f7a2481e5f742babd5961260a9f019
Jacob De Hoyos <dehoyos.jacob@gmail.com>
2021-12-10 03:32:35 -0600

    Further progress on menu
    Import font for game, add text to menu and create type effect for the prompter.

---
+++ Assets/Scipts/TypeEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeEffect : MonoBehaviour
+{
+    [SerializeField] float delay = 0.1f;
+    [SerializeField] string fullText;
+
+    string currentText = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        for(int i = 0; i <= fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0,i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
