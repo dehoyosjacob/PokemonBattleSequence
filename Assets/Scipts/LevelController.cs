75f34b4ff7caa08b294c0820a80d82207d4572cf
Jacob De Hoyos <dehoyos.jacob@gmail.com>
2021-12-17 02:38:44 -0600

    Finish general battle UI
    Make sure that everything in the battle UI is fully functional.

--- Assets/Scipts/LevelController.cs
+++ Assets/Scipts/LevelController.cs
@@ -7,17 +7,23 @@ public class LevelController : MonoBehaviour
 {
     [SerializeField] GameObject menuPanel;
     [SerializeField] GameObject fightPanel;
+    [SerializeField] GameObject bagPanel;
     [SerializeField] Text firstText;
     [SerializeField] Text secondText;
 
     public ArrowControl _menuArrows;
+
+    //test variables
+    bool pokemonTest;
+    bool bagTest;
+    bool runTest;
 
     void Update()
     {
         if(Input.GetKeyUp(KeyCode.Return))
         {
             Debug.Log("Pressed Enter");
-            if(!menuPanel.activeSelf)
+            if(firstText.gameObject.activeSelf && !menuPanel.activeSelf)
             {
                 BeginBattle();
             }
@@ -25,6 +31,14 @@ public class LevelController : MonoBehaviour
             else if(menuPanel.activeSelf)
             {
                 MenuSelect();
+            }
+        }
+
+        else if(Input.GetKeyUp(KeyCode.Backspace))
+        {
+            if (!firstText.gameObject.activeSelf)
+            {
+                BackToMenu();
             }
         }
     }
@@ -44,5 +58,50 @@ public class LevelController : MonoBehaviour
             secondText.gameObject.SetActive(false);
             fightPanel.SetActive(true);
         }
+
+        else if(_menuArrows.bottomLeft.gameObject.activeSelf)
+        {
+            Debug.Log("Pokemon menu selected!");
+            pokemonTest = true;
+        }
+
+        else if (_menuArrows.topRight.gameObject.activeSelf)
+        {
+            Debug.Log("Bag menu selected!");
+            //bagTest = true;
+            menuPanel.SetActive(false);
+            fightPanel.SetActive(false);
+            bagPanel.SetActive(true);
+        }
+
+        else if (_menuArrows.bottomRight.gameObject.activeSelf)
+        {
+            Debug.Log("Run menu selected!");
+            runTest = true;
+        }
+    }
+
+    void BackToMenu()
+    {
+        if(fightPanel.activeSelf)
+        {
+            fightPanel.SetActive(false);
+        }
+
+        else if(pokemonTest)
+        {
+            Debug.Log("Exit the pokemon menu");
+            pokemonTest = false;
+        }
+
+        else if(bagPanel.activeSelf)
+        {
+            Debug.Log("Exit the bag menu");
+            //bagTest = false;
+            bagPanel.SetActive(false);
+        }
+
+        menuPanel.SetActive(true);
+        secondText.gameObject.SetActive(true);
     }
 }
