de1a2b4a1583dcd82fc9723211d80cabec39a729
Jacob De Hoyos <dehoyos.jacob@gmail.com>
2021-12-24 18:00:51 -0600

    Pokemon Data Holder
    Create a script that stores all necessary data about a pokemons stats

---
+++ Assets/Scipts/PokemonDataHolder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonDataHolder : MonoBehaviour
+{
+    [SerializeField] int pokemonNumber;
+    [SerializeField] int lvl;
+    [SerializeField] int maxHealth;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int specialAttack;
+    [SerializeField] int specialDefense;
+    [SerializeField] int speed;
+}
